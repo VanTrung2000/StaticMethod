7a42867dd386a58fa8587ba80ba9dcd86223b35f
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-21 10:21:08 +0700

    Add project files.

---
+++ StaticMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Studen.Change();
+            Studen s1 = new Studen(12, "trung");
+            Studen s2 = new Studen(13, "trung1");
+            s1.Display();
+            s2.Display();
+        }
+    }
+}
---
+++ StaticMethod/Studen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticMethod
+{
+    class Studen
+    {
+        private int rollno;
+        private string name;
+        private static string college = "BBDIT";
+
+        public Studen(int r, string n)
+        {
+            rollno = r;
+            name = n;
+        }
+        public static void Change()
+        {
+            college = "PHUXUAN";
+        }
+        public void Display()
+        {
+            Console.WriteLine(rollno + "  " + name + "  " + college);
+        }
+    }
+}
